0192d4dd17ee8e517af3080f05b3442f81ddd2f7
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-06-11 14:48:43 +0200

    modifies server.js to use cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const userController = require("./userController");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Use CORS middleware
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use("/user", userController);
