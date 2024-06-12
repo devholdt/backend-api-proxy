2373d7bf5773e91a7078162ef8f49e820f6bb87c
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-06-12 15:43:16 +0200

    fixes file path for database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./db");
+const connectDB = require("./database/db");
 const app = express();
 const userRoutes = require("./routes/userRoutes");
 
